fde8c37fd16f518ac1633a20089f0e4032472d9f
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2024-11-07 14:36:24 +0500

    GrillUp!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
-import Image from "next/image";
-import Navbar from "../app/grillUp/Navbar"
+import Image from "next/image";  // Commented out since it's not used currently
+import Navbar from "../app/grillUp/Navbar";
 import Screen from "./grillUp/Screen";
-import About from "../app/grillUp/About"
-import Menu from "../app/grillUp/MenuSection"
-import Offer from "../app/grillUp/offers"
-import  Contact from "./grillUp/Contact"
+import About from "../app/grillUp/About";
+import Menu from "../app/grillUp/MenuSection";
+import Offer from "../app/grillUp/offers";
+import Contact from "./grillUp/Contact";
 import Footer from "./grillUp/Footer";
+
 export default function Home() {
   return (
-  <>
-  <Navbar/>
-  <Screen/>
-  <About/>
-  <Menu/>
- <Offer/>
-  <Contact/>
-  <Footer/>
-  </>
+    <>
+      <Navbar />
+      <Screen />
+      <About />
+      <Menu />
+      <Offer />
+      <Contact />
+      <Footer />
+    </>
   );
 }
